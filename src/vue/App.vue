d8258da39d9603900402847fc20de73e3b574fe6
Andrew Kurano <andrew.kurano@triggerglobal.com>
2021-01-31 00:55:37 -0800

    Added ghost modes

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -1,23 +1,29 @@
 <template>
-    <span class = "rowheader">Possiblities:</span>
-    <div style = "display: flex">
-      <clue v-for = "(item, index) in remainingghosts" :title="item"></clue>
+  <div>
+    <div v-if = "isShowingGhosts">
+      <span class = "rowheader">Possiblities:</span>
+      <div style = "display: flex">
+        <clue v-for = "(item, index) in remainingghosts" :title="item"></clue>
 
+      </div>
+      <br>
     </div>
-    <br>
 
+    <div>
+      <span class = "rowheader">Found: </span>
+      <div style = "display: flex">
+        <clue v-for = "(item, index) in foundclues" :title="item"></clue>
 
-    <span class = "rowheader">Found: </span>
-  <div style = "display: flex">
-    <clue v-for = "(item, index) in foundclues" :title="item"></clue>
+      </div>
+      <br>
+    </div>
 
-  </div>
-    <br>
-
-    <span class = "rowheader">Looking For: </span>
-  <div style = "display: flex">
-    <clue v-for = "(item, index) in possibleclues" :title="item"></clue>
-
+    <div>
+      <span class = "rowheader">Looking For: </span>
+      <div style = "display: flex">
+        <clue v-for = "(item, index) in possibleclues" :title="item"></clue>
+      </div>
+    </div>
 
   </div>
 </template>
@@ -33,7 +39,13 @@ export default {
   },
   data: function() {
     return {
-      alwaysshow: false,
+      ghostRevealModes: {
+        'ALWAYS': 0,
+        'ONFIRSTCLUE': 1,
+        'ONLASTCLUE': 2,
+        'NEVER': 3
+      },
+      ghostRevealMode: 0,
       username: "vanessienessie",
       clues: ["orbs", "freezing", "fingerprints", "box", "writing", "emf"],
       ghosts: {
@@ -60,6 +72,11 @@ export default {
     this.resetGhosts();
 
     ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
+      let args = [];
+      if(message != ""){
+        args = message.split(' ');
+      }
+
       if(flags.mod || flags.broadcaster || flags.vip){
 
         if (this.clues.includes(command)) {
@@ -68,12 +85,43 @@ export default {
         } else if (command === "phasmo-reset") {
           this.resetClues();
           this.resetGhosts();
+        } else if (command === "phasmo-ghostmode") {
+          if(args.length > 0){
+            let ghostMode = args[0].toUpperCase();
+            console.log(ghostMode)
+            if(this.ghostRevealModes[ghostMode] != null){
+              console.log(this.ghostRevealModes[ghostMode]);
+              this.ghostRevealMode =  this.ghostRevealModes[ghostMode];
+            }
+          }
         }
       }
     }
     ComfyJS.Init( "vanessienessie" );
   },
+  computed: {
+    isShowingGhosts(){
+      var isShowing = false;
+      switch(this.ghostRevealMode){
+        case this.ghostRevealModes['ALWAYS']:
+          isShowing = true;
+          break;
+        case this.ghostRevealModes['ONFIRSTCLUE']:
+          if(this.foundclues.length >= 1){
+            isShowing = true;
+          }
+          break;
+        case this.ghostRevealModes['ONLASTCLUE']:
+          if(this.foundclues.length >= 2){
+            isShowing = true;
+          }
+          break;
+      }
+      return isShowing;
+    }
+  },
   methods: {
+
     addClue(command){
       this.foundclues.push(command);
       this.possibleclues.splice(this.possibleclues.indexOf(command), 1);
@@ -92,8 +140,6 @@ export default {
           }
         }
       }
-
-
     },
     resetGhosts(){
       this.remainingghosts = [];
